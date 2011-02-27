6189e7c8a32681a7aa4b61457b7e87ce962cbec8
Curtis Bridges <curtis.bridges@gmail.com>
2011-02-27 11:20:50 -0500

    Cleanup the code.

--- src/com/curtisbridges/Tester.java
+++ src/com/curtisbridges/Tester.java
@@ -3,17 +3,17 @@ package com.curtisbridges;
 import java.io.Serializable;
 
 public class Tester implements Serializable {
-	private Object data;
-	
-	public Tester(Object obj) {
-		data = obj;
-	}
-	
-	public Object getData() {
-		return data;
-	}
-	
-	public String toString() {
-		return "[Tester]" + data.toString();
-	}
+    private Object data;
+
+    public Tester(Object obj) {
+        data = obj;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public String toString() {
+        return "[Tester]" + data.toString();
+    }
 }
